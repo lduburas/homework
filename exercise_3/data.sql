7f2a83b229c3acb9c287406483fe4ec130043246
Laurynas Duburas <laduem@gmail.com>
2025-05-23 21:28:58 +0300

    fixed dates in test data

--- exercise_3/data.sql
+++ exercise_3/data.sql
@@ -1,25 +1,25 @@
 insert into 
   INVOICE(INVOICE_ID, INVOICE_DATE, INVOICE_AMOUNT, CURRENCY)
 values
- (1, SYSDATE, 150, 'EUR'),
- (2, add_months(SYSDATE, -1), 150, 'EUR'),
- (3, SYSDATE - 10, 500, 'USD'),
- (4, SYSDATE - 10, 503, 'EUR'),
- (5, SYSDATE - 7, 700, 'EUR');
+ (1, TO_DATE('23/05/25'), 150, 'EUR'),
+ (2, TO_DATE('23/04/25'), 150, 'EUR'),
+ (3, TO_DATE('13/05/25'), 500, 'USD'),
+ (4, TO_DATE('13/05/25'), 503, 'EUR'),
+ (5, TO_DATE('16/05/25'), 700, 'EUR');
 
 
 insert into 
   PAYMENT(PAYMENT_ID, PAYMENT_DATE, PAYMENT_AMOUNT, CURRENCY, INVOICE_ID)
 values
- ( 1, SYSDATE - 3,  50, 'EUR', 1),
- ( 2, SYSDATE - 3,  57, 'EUR', 1),
- ( 3, SYSDATE - 3,  32, 'EUR', 2),
- ( 4, SYSDATE - 2,   7, 'EUR', 3),
- ( 5, SYSDATE - 2, 493, 'USD', 3),
- ( 6, SYSDATE - 2, 400, 'EUR', 4),
- ( 7, SYSDATE - 1,  32, 'EUR', 4),
- ( 8, SYSDATE - 1,   1, 'EUR', 4),
- ( 9, SYSDATE - 1,  70, 'EUR', 4),
- (10, SYSDATE,     350, 'EUR', 5),
- (11, SYSDATE,     350, 'EUR', 5),
- (12, SYSDATE,      43, 'EUR', 1);
+ ( 1, TO_DATE('20/05/25'),  50, 'EUR', 1),
+ ( 2, TO_DATE('20/05/25'),  57, 'EUR', 1),
+ ( 3, TO_DATE('20/05/25'),  32, 'EUR', 2),
+ ( 4, TO_DATE('21/05/25'),   7, 'EUR', 3),
+ ( 5, TO_DATE('21/05/25'), 493, 'USD', 3),
+ ( 6, TO_DATE('21/05/25'), 400, 'EUR', 4),
+ ( 7, TO_DATE('22/05/25'),  32, 'EUR', 4),
+ ( 8, TO_DATE('22/05/25'),   1, 'EUR', 4),
+ ( 9, TO_DATE('22/05/25'),  70, 'EUR', 4),
+ (10, TO_DATE('23/05/25'), 350, 'EUR', 5),
+ (11, TO_DATE('23/05/25'), 350, 'EUR', 5),
+ (12, TO_DATE('23/05/25'),  43, 'EUR', 1);

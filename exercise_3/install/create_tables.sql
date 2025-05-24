61508dea915ad6b731fcf94b8a5a551af3a664e4
Laurynas Duburas <laduem@gmail.com>
2025-05-24 16:31:03 +0300

    table tuning

--- exercise_3/install/create_tables.sql
+++ exercise_3/install/create_tables.sql
@@ -2,18 +2,18 @@ drop table if exists PAYMENT cascade constraints;
 drop table if exists INVOICE cascade constraints;
 
 create table PAYMENT (
-  PAYMENT_ID NUMBER,
-  PAYMENT_DATE DATE,
-  PAYMENT_AMOUNT NUMBER,
-  CURRENCY VARCHAR2(3),
-  INVOICE_ID NUMBER
+  PAYMENT_ID NUMBER NOT NULL,
+  PAYMENT_DATE DATE NOT NULL,
+  PAYMENT_AMOUNT NUMBER NOT NULL,
+  CURRENCY VARCHAR2(3) NOT NULL,
+  INVOICE_ID NUMBER NOT NULL
 );
 
 create table INVOICE (
-INVOICE_ID NUMBER,
-INVOICE_DATE DATE,
-INVOICE_AMOUNT NUMBER,
-CURRENCY VARCHAR2(3)
+  INVOICE_ID NUMBER NOT NULL,
+  INVOICE_DATE DATE NOT NULL,
+  INVOICE_AMOUNT NUMBER NOT NULL,
+  CURRENCY VARCHAR2(3) NOT NULL
 );
 
 alter table PAYMENT add constraint PAYMENT_PK primary key (PAYMENT_ID);
@@ -24,7 +24,7 @@ alter table PAYMENT add constraint PAYMENT_FK foreign key (INVOICE_ID) reference
 
 
 drop sequence if exists PAYMENT_SEQ;
-create sequence PAYMENT_SEQ start with 1;
+create sequence PAYMENT_SEQ start with 100;
 
 drop sequence if exists INVOICE_SEQ;
-create sequence INVOICE_SEQ start with 1;
+create sequence INVOICE_SEQ start with 100;
